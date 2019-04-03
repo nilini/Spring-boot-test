7e92db6efa2927684daf46808b788d897b0e368a
WangSai <1204222505@qq.com>
2019-04-03 14:52:23 +0800

    Spring MVC ModelAndView

--- src/main/java/com/springboottest/example/Controller/IndexController.java
+++ src/main/java/com/springboottest/example/Controller/IndexController.java
@@ -3,6 +3,7 @@ package com.springboottest.example.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -14,5 +15,13 @@ public class IndexController {
         model.addAttribute("title", "this is title");
         // 返回resources/templates/index.html
         return "index";
+    }
+
+    @RequestMapping("/modelView")
+    public ModelAndView index2(ModelAndView modelAndView){
+        System.out.println("IndexController.index2");
+        System.out.println(modelAndView);
+        modelAndView.setViewName("redirect:/");
+        return modelAndView;
     }
 }
